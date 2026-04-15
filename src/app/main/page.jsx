6d0421f341673779acb33c5c7e6bf4b75c162fc1
sohan24-dev <sohan.explorer@gmail.com>
2026-04-15 11:50:38 +0600

    add cache

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,6 +1,8 @@
 import MainData from "@/components/MainData/MainData";
 const Data = async () => {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("http://localhost:3000/data.json",{
+    cache:'no-store'
+  });
   const data = await res.json();
   return data
 }
